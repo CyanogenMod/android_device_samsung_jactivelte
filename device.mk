f50c814b432b6cd84dbc83ef12048cbe2159c09b
Arun Manohar Manoharan <email2arunmanohar@gmail.com>
2014-06-24 19:14:13 +0530

    Update vendor path to TheMuppets repo.

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jactiveltexx/jactiveltexx-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/jactivelte/jactivelte-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jactiveltexx/overlay
