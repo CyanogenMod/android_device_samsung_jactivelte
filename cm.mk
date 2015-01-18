af2ae10231e6d92c2bc8dd6cf2d7b511b8ff443b
Spegelius <spegelius@gmail.com>
2015-01-18 12:01:24 +0200

    Fixed android ver in PRIVATE_BUILD_DESC prop
    Change-Id: I40a35d250cf068ab837e81098933093e434b9a37

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jactivelte TARGET_DEVICE=jactivelte \
     TARGET_DEVICE=jfltespr \
     BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:4.4.2/KTU84Q/I9295XXUCNE5:user/release-keys" \
-    PRIVATE_BUILD_DESC="jactivelte-user 4.4.4 KTU84Q I9295XXUCNE5 release-keys"
+    PRIVATE_BUILD_DESC="jactivelte-user 4.4.2 KTU84Q I9295XXUCNE5 release-keys"
 
 PRODUCT_NAME := cm_jactivelte
 PRODUCT_DEVICE := jactivelte
