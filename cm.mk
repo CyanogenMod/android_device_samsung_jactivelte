154ca861317b948bcba40b06407c4ddcd84f9856
Spegelius <spegelius@gmail.com>
2015-01-31 23:34:48 +0200

    Removed duplicate TARGET_DEVICE
    Change-Id: I5419b4ba575bdb8911785e9792520036f633eaf7

--- cm.mk
+++ cm.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=jactivelte TARGET_DEVICE=jactivelte \
+    PRODUCT_NAME=jactivelte \
     TARGET_DEVICE=jactivelte \
     BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:4.4.2/KTU84Q/I9295XXUCNE5:user/release-keys" \
     PRIVATE_BUILD_DESC="jactivelte-user 4.4.2 KTU84Q I9295XXUCNE5 release-keys"
